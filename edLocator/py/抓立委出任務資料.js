8afbd5f4674282fa625eabca647c760809277474
tommy87166 <tommy87166@gmail.com>
2015-11-28 04:26:20 +0800

    reset

--- edLocator/py/抓立委出任務資料.js
+++
@@ -1,6 +0,0 @@
-var data= Object.values(window.__data.legislators);
-var legislator = {};
-for (var x=0;x<data.length;x++){
- legislator[data[x].name] = data[x].id;
-}
-console.log(JSON.stringify(legislator));
